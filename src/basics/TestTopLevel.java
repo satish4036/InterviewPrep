d0442dad6bd985d85657c2bda1b6cd642c4ba7e0
satish4036 <satish4036@users.noreply.github.com>
2020-07-24 20:28:24 +0530

    Converted to Java Project

---
+++ src/basics/TestTopLevel.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class TestTopLevel {
+
+	public static void main(String[] args) {
+		
+	System.out.println("This class holds two top level classes but one only public");
+
+	}
+	
+	
+
+}
+
+ class Test {
+	
+}
